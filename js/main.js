04d4acb16ea77b644d604e5fc7a65760b1af9b30
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-04-16 16:25:43 -0600

    scripts!

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function() {
+document.addEventListener("DOMContentLoaded", function(){
+		Typed.new(".typed-element", {
+			strings: ["Trust me... ^1000I'm an engineer"],
+			typeSpeed: 0,
+            showCursor: false
+		});
+	});
+})();
